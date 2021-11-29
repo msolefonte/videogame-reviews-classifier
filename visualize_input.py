e6dc2487f8f436a8cb6ad0db2ca1185dd7579391
msolefonte <msolefonte@gmail.com>
2021-11-29 12:24:47 +0000

    Modify dataset retrieving

--- visualize_input.py
+++ visualize_input.py
@@ -1,16 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import lib
+from lib.loader import get_data
+from lib.utils.common import calculate_distribution
 
-x, y, names = lib.get_data()
-
-def calculate_distribution(y):
-    dis = np.zeros((11), np.int64)
-
-    for value in y:
-        dis[value - 1] = dis[value - 1] + 1
-
-    return dis
+x, y, names = get_data()
 
 plt.xlabel('Rating')
 plt.ylabel('Number of reviews')
@@ -20,5 +13,5 @@ plt.bar(bar_range - 0.35 / 2, distribution, width=0.35,
         label='Input Data', tick_label=range(len(distribution)))
 
 plt.legend()
-plt.savefig(f'input_data_distribution.jpg')
+plt.savefig(f'images/input_data_distribution.jpg')
 plt.clf
