7af9074134a73de3ef5ffa63073a4bc41396f191
kjacob15 <91503607+kjacob15@users.noreply.github.com>
2021-12-05 14:27:18 +0000

    knn changes

--- knn_baseline.py
+++ knn_baseline.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score, mean_absolute_error, confusion_matrix, classification_report
+from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsRegressor
 from lib.loader import get_data
@@ -28,12 +28,13 @@ def average(sum):
 
 
 def run(neighbours):
-    knn_neighbours = KNeighborsRegressor(n_neighbors=neighbours, weights='distance')
+    knn_neighbours = KNeighborsRegressor(
+        n_neighbors=neighbours, weights='distance')
 
     mean_absolute_error_train = 0
-    accuracy_train = 0
+    score_train = 0
     mean_absolute_error_test = 0
-    accuracy_test = 0
+    score_test = 0
     for train_idx, test_idx in kf.split(x):
         x_train = x[train_idx]
         y_train = y[train_idx]
@@ -46,58 +47,59 @@ def run(neighbours):
         # print(y_pred)
         mean_absolute_error_train = mean_absolute_error_train + \
             mean_absolute_error(y_pred, y_train)
-        accuracy_train= accuracy_train + knn_neighbours.score(x_train,y_train)
+        score_train = score_train + \
+            knn_neighbours.score(x_train, y_train)
 
         y_pred = knn_neighbours.predict(x_test)
 
         mean_absolute_error_test = mean_absolute_error_test + \
             mean_absolute_error(y_pred, y_test)
-        accuracy_test= accuracy_train + knn_neighbours.score(x_test,y_test)
+        score_test = score_train + knn_neighbours.score(x_test, y_test)
 
-    accuracy_train = average(accuracy_train)
-    accuracy_test = average(accuracy_test)
+    score_train = average(score_train)
+    score_test = average(score_test)
     mean_absolute_error_test = average(mean_absolute_error_test)
     mean_absolute_error_train = average(mean_absolute_error_train)
 
     print(
-        f'Training accuracy={accuracy_train} \n Test Accuracy={accuracy_test}')
+        f'Training accuracy={score_train} \n Test Accuracy={score_test}')
     print(
         f'Mean Absolute Error Training={mean_absolute_error_train} \n Mean Absolute Error Test={mean_absolute_error_test}')
 
     plt.xlabel('score')
     plt.ylabel('Number of reviews')
 
-    distribution = calculate_distribution(y_pred)
+    distribution, xticklables = calculate_distribution(y_pred)
     bar_range = np.array(range(len(distribution)))
-    plt.bar(bar_range - 0.35 / 2, distribution, width=0.35,
-            label='train', tick_label=range(len(distribution)))
+    plt.bar(bar_range-0.35/2, distribution, width=0.35,
+            label='Predicted', tick_label=range(len(distribution)))
 
-    distribution = calculate_distribution(y_test)
-    plt.bar(bar_range + 0.35 / 2, distribution, width=0.35,
-            label='test', tick_label=range(len(distribution)))
+    distribution, xticklables = calculate_distribution(y_test)
+    plt.bar(bar_range+0.35/2, distribution, width=0.35,
+            label='Real', tick_label=range(len(distribution)))
 
     plt.legend()
     plt.savefig(f'images/knn_{neighbours}.jpg')
     plt.clf()
 
-    print(accuracy_train, accuracy_test,
+    print(score_train, score_test,
           mean_absolute_error_train, mean_absolute_error_test)
-    return accuracy_train, accuracy_test, mean_absolute_error_train, mean_absolute_error_test
+    return score_train, score_test, mean_absolute_error_train, mean_absolute_error_test
 
 
 def main():
     neighbours = [10, 20, 30, 50, 65, 70]
-    accuracy_train = [0.0] * len(neighbours)
-    accuracy_test = [0.0] * len(neighbours)
+    score_train = [0.0] * len(neighbours)
+    score_test = [0.0] * len(neighbours)
     mean_absolute_error_train = [0.0] * len(neighbours)
     mean_absolute_error_test = [0.0] * len(neighbours)
 
     for i in range(len(neighbours)):
-        accuracy_train[i], accuracy_test[i], mean_absolute_error_train[i], mean_absolute_error_test[i] = run(
+        score_train[i], score_test[i], mean_absolute_error_train[i], mean_absolute_error_test[i] = run(
             neighbours[i])
 
     plt.xlabel('Neighbours')
-    plt.ylabel('mean average error')
+    plt.ylabel('mean absolute error')
     plt.plot(neighbours, mean_absolute_error_train, label='train')
     plt.plot(neighbours, mean_absolute_error_test, label='test')
     plt.legend()
@@ -106,8 +108,8 @@ def main():
 
     plt.xlabel('Neighbours')
     plt.ylabel('Score')
-    plt.plot(neighbours, accuracy_train, label='train')
-    plt.plot(neighbours, accuracy_test, label='test')
+    plt.plot(neighbours, score_train, label='train')
+    plt.plot(neighbours, score_test, label='test')
     plt.legend()
     plt.savefig('images/knn_vs_score.jpg')
     plt.clf()
